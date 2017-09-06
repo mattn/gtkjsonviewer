a98bff2296d0597cf44e9f530d71325e218fb938
odrevet <olivier.drevet@cycleco.eu>
2017-09-06 15:40:10 +0200

    display current path of the selected element (the path can be used with jq)

--- gtkjsonview.py
+++ gtkjsonview.py
@@ -16,37 +16,41 @@ else:
 if raw_data[0] == '(' and raw_data[-1] == ')':
   raw_data = raw_data[1:-1]
 
-
-color_type = 'blue'
+color_array = 'yellow'
+color_type = 'orange'
 color_string = 'pink'
 color_integer = 'red'
 color_object = 'yellow'
-color_array = 'orange'
 color_key = 'light green'
 
 def add_item(key, data, model, parent = None):
   if isinstance(data, dict):
     if len(key):
       obj = model.append(parent, ['<span foreground="'+color_object+'">'
-                                  + str(key) + '</span>' + ' <b>{}</b>'])
+                                  + str(key) + '</span>' +
+                                  ' <span foreground="'+color_type+'"><b>{}</b></span>'])
       walk_tree(data, model, obj)
     else:
       walk_tree(data, model, parent)
   elif isinstance(data, list):
-    arr = model.append(parent, ['<span foreground="'+color_array+'">"' + key + '"</span>' +' <b>[]</b>'])
+    arr = model.append(parent, ['<span foreground="'+color_array+'">'+ key + '</span> '
+                                '<span foreground="'+color_type+'"><b>[]</b></span>'])
     for index in range(0, len(data)):
-      add_item('', data[index], model, model.append(arr, ['<b>[</b>' + str(index) + '<b>]</b>']))
+      add_item('', data[index], model, model.append(arr, ['<b><span foreground="'+color_type+'">'+'['+'</span></b><span foreground="'+color_integer+'">' + str(index) + '</span><b><span foreground="'+color_type+'">]</span></b>']))
   elif isinstance(data, str):
     if len(data) > 256:
       data = data[0:255] + "..."
       model.append(parent, ['<span foreground="'+color_key+'">"' + key + '"</span>' +
                             '<b>:</b> <span foreground="'+color_string+'">"' + data + '"</span>'])
     else:
-      model.append(parent, ['<span foreground="'+color_key+'">"' + key + '"</span>' +
-                            '  <b>:</b> <span foreground="'+color_string+'">"' + data + '"</span>'])
+      if key:
+        model.append(parent, ['<span foreground="'+color_key+'">"' + key + '"</span>' +
+                              '  <b>:</b> <span foreground="'+color_string+'">"' + data + '"</span>'])
+      else:
+        model.append(parent, ['<span foreground="'+color_string+'">"' + data + '"</span>'])
   elif isinstance(data, int):
     model.append(parent, ['<span foreground="'+color_key+'">"' + key + '"</span>' +
-                          '  <b>:</b> <span foreground="'+color_string+'">' + str(data) + '</span>'])
+                          '  <b>:</b> <span foreground="'+color_integer+'">' + str(data) + '</span>'])
   else:
     model.append(parent, [str(data)])
 
@@ -59,23 +63,78 @@ def walk_tree(data, model, parent = None):
   else:
     add_item('', data, model, parent)
 
-data = json.loads(raw_data)
+#return the json query given a path
+def to_jq(path, data):
+  indices = path.get_indices()
+  depth  = path.get_depth()
+  jq = ''
+  depth_current = 0
+  is_array_index = False
+
+  #the expression must begin with identity `.`
+  #if the first element is not a dict, add a dot
+  if not isinstance(data, dict):
+    jq += '.'
+
+  while depth_current < depth:
+    if isinstance(data, dict):
+      key = (list(data)[indices[depth_current]])
+      jq += '.' + key
+      data = data[key]
+      if isinstance(data, list):
+        jq += '[]'
+        is_array_index = True
+    elif isinstance(data, list):
+      if is_array_index:
+        selected_index = indices[depth_current]
+        jq = jq[:-2]   #remove []
+        jq += '[{}]'.format(selected_index)
+        data = data[selected_index]
+        is_array_index = False
+      else:
+        jq += '[]'
+        is_array_index = True
+
+    depth_current += 1
+
+  return jq
 
 class JSONViewerWindow(Gtk.Window):
     def __init__(self):
       Gtk.Window.__init__(self, title="JSon Viewer")
       self.set_default_size(600, 400)
-      swin = Gtk.ScrolledWindow()
+
+      self.label_path = Gtk.Label()
+      self.label_path.set_selectable(True)
+
+      self.data = json.loads(raw_data)
+
+      swintree = Gtk.ScrolledWindow()
+      swinpath = Gtk.ScrolledWindow()
       model = Gtk.TreeStore(str)
       tree = Gtk.TreeView(model)
       cell = Gtk.CellRendererText()
       tvcol = Gtk.TreeViewColumn('JSON', cell, markup=0)
+
+      tree_selection = tree.get_selection()
+      tree_selection.connect("changed", self.on_selection_changed)
       tree.append_column(tvcol)
-      swin.add(tree)
-      self.add(swin)
-      walk_tree(data, model)
 
+      box = Gtk.Box(orientation=Gtk.Orientation.VERTICAL)
+      box.pack_start(swintree, True, True, 1)
+      box.pack_start(swinpath, False, False, 1)
+      swintree.add(tree)
+      swinpath.add(self.label_path)
+      self.add(box)
 
+      walk_tree(self.data, model)
+
+    def on_selection_changed(self, tree_selection) :
+      (model, iter_current) = tree_selection.get_selected()
+      path = model.get_path(iter_current)
+      jq = to_jq(path, self.data)
+      jq_str = ''.join(jq)
+      self.label_path.set_text(jq_str)
 
 win = JSONViewerWindow()
 win.connect("delete-event", Gtk.main_quit)
