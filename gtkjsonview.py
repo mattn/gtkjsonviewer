mattn/gtkjsonviewer

commit 172f3264b4b60d5c6d2c836b8b8a072577662b7c

Show dictionary/object keys in sorted order rather than Python random hash order

gtkjsonview.py
@@ -78,7 +78,7 @@ def walk_tree(data, model, parent = None):
   if isinstance(data, list):
     add_item('', data, model, parent)
   elif isinstance(data, dict):
-    for key in data:
+    for key in sorted(data):
       add_item(key, data[key], model, parent)
   else:
     add_item('', data, model, parent)

commit 19da62aa24fde171323ff2b50b88353dec1495f8

Fix JSON path expression bug introduced by dictionary sorting
Bug introduced in commit 172f3264b4b60d5c6d2c836b8b8a072577662b7c

When iterating over a dictionary to figure out which item is the
selected one in the tree, the iteration must be done in the same
key order as that used for displaying the tree.

gtkjsonview.py
@@ -96,7 +96,7 @@ def to_jq(path, data):
 
   for index in indices:
     if isinstance(data, dict):
-      key = (list(data)[index])
+      key = (list(sorted(data))[index])
       jq += '.' + key
       data = data[key]
       if isinstance(data, list):